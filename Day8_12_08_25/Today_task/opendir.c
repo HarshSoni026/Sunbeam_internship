81170a21398ba8d191da6d2f18d1a82940e837e6
E1_83818_Harsh <soniharsh026@gmail.com>
2025-08-14 09:15:23 +0530

    Day8_task_&_content

---
+++ Day8_12_08_25/Today_task/opendir.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <dirent.h>
+
+int main() {
+    DIR *d;
+    struct dirent *dir;
+
+    d = opendir("/home/harsh/0324desd/Internship");
+    if(!d)
+        perror("opendir");
+    
+    while ((dir = readdir(d)) != NULL) {
+        printf("%s\n", dir->d_name);
+    }
+    closedir(d);
+            
+    return 0;
+}
